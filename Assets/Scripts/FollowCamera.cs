6cf88452b1c18f62fd84c2299190bca4c253393c
Brandon Lewis <brandon.lewis477@comcast.net>
2020-07-17 20:37:06 -0500

    Added rudimentary mouse and keyboard controls

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public static int sensitivity = 2;
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(player.transform.position,player.transform.right,-1 * sensitivity * Input.GetAxis("Mouse Y"));
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical")).normalized;
+        transform.Translate(inputDirection * speed);
+        transform.Rotate(FollowCamera.sensitivity * Input.GetAxis("Mouse X") * Vector3.up,Space.Self);
+    }
+}
